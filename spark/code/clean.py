a9424bb140b83cac5de88c6254955ae008319c9d
SO4REN <ale.catinello@gmail.com>
2023-06-12 17:20:14 +0200

    feat: parse csv to dataframe in streaming

--- spark/code/clean.py
+++ spark/code/clean.py
@@ -1,33 +1,15 @@
 import os
 import pyspark.sql.types as tp
+import pyspark.sql.functions as fun
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.session import SparkSession
-from pyspark.sql.functions import from_json, col, udf
 
 
-# def cleanCSV(data):
-#     schema = tp.StructType([
-#         tp.StructField(name= "idImpianto", dataType= tp.IntegerType(), nullable= False),
-#         tp.StructField(name= "descCarburante", dataType= tp.StringType(), nullable= False),
-#         tp.StructField(name= "prezzo", dataType= tp.FloatType(), nullable= False),
-#         tp.StructField(name= "isSelf", dataType= tp.IntegerType(), nullable= False),
-#         tp.StructField(name= "dtComu", dataType= tp.DateType(), nullable= False)
-#     ])
-    
-#     datasetFolder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dataset")
-#     anagrafica = spark.read.parquet(os.path.join(datasetFolder, "anagrafica_impianti_CT.parquet"))
-    
-#     prezziDF = spark.read.csv(data, schema= schema, sep=";")
-#     prezziDF = prezziDF[prezziDF.isSelf == 1]
-#     prezziDF = prezziDF[prezziDF.descCarburante.isin(["Benzina", "Gasolio"])]
-#     prezziDF = prezziDF.withColumnRenamed("descCarburante", "carburante")
-#     prezziDF = prezziDF.withColumnRenamed("idImpianto", "idImpiantoPrezzo")
 
-#     df = prezziDF.join(anagrafica, prezziDF.idImpiantoPrezzo == anagrafica.idImpianto, how="inner")
-#     df = df.drop("__index_level_0__")
-#     df = df.drop("isSelf")
-#     df = df.drop("idImpiantoPrezzo")
-#     df = df.drop("dtComu")
+
+def removeParenthesis(string):
+    string = string[1:-1]
+    return string.split("\n", 2)[2]
 
 
 
@@ -40,13 +22,22 @@ sparkConf = SparkConf()
 sc = SparkContext(appName = "Clean", conf = sparkConf)
 spark = SparkSession(sc)
 
-schema = tp.StructType([
+schemaJSON = tp.StructType([
         tp.StructField(name= "@timestamp", dataType= tp.DateType(), nullable= False),
-        tp.StructField(name = "event", dataType= tp.StructType([tp.StructField(name="original", dataType= tp.StringType(), nullable= False)])),
+        tp.StructField(name = "event", dataType= 
+                       tp.StructType([tp.StructField(name="original", dataType= tp.StringType(), nullable= False)])
+                       ),
         tp.StructField(name= "hash", dataType= tp.StringType(), nullable= False),
         tp.StructField(name= "column1", dataType= tp.StringType(), nullable= False),
         tp.StructField(name= "@version", dataType= tp.IntegerType(), nullable= False)
     ])
+
+
+datasetFolder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dataset")
+anagrafica = spark.read.parquet(os.path.join(datasetFolder, "anagrafica_impianti_CT.parquet"))
+
+
+#! --------------- READ STREAM FROM KAFKA -------------------
 
 inputDF = spark.readStream.format("kafka") \
         .option("kafka.bootstrap.servers", kafkaServer) \
@@ -54,35 +45,33 @@ inputDF = spark.readStream.format("kafka") \
         .load()
 
 df = inputDF.selectExpr("CAST(value AS STRING)") \
-        .select(from_json(col("value"), schema).alias("data")) \
+        .select(fun.from_json(fun.col("value"), schemaJSON).alias("data")) \
         .select("data.event", "data.hash", "data.@timestamp")
 df = df.withColumnRenamed("@timestamp", "timestamp")
+df = df.drop_duplicates(["hash"])
+
+preClean = fun.udf(removeParenthesis, tp.StringType())
+df.event = df.event.cast("string")
+df = df.withColumn("event", preClean(df.event))
+
+csv = df.select(fun.explode(fun.split(df.event, "\n")).alias("csv"))
+csv = csv.select(fun.from_csv(fun.col("csv"),
+                schema="idImpianto INT, descCarburante STRING, prezzo DOUBLE, isSelf INT, dtComu STRING",
+                options={"sep" : ';'}).alias("data")).select("data.*")
+
+csv = csv[csv.isSelf == 1]
+csv = csv[csv.descCarburante.isin(["Benzina", "Gasolio"])]
+csv = csv.withColumnRenamed("descCarburante", "carburante")
+csv = csv.withColumnRenamed("idImpianto", "idImpiantoPrezzo")
+csv = csv.join(anagrafica, csv.idImpiantoPrezzo == anagrafica.idImpianto, how="inner")
+
+csv = csv.drop("__index_level_0__", "isSelf", "idImpianto", "Provincia", "dtComu", "idImpiantoPrezzo")
 
 
-df.writeStream \
+csv.writeStream \
     .format("console") \
     .start() \
     .awaitTermination()
 
-
-
-
-
-
-
-
-
-# i = 0
-# hashes = dict.fromkeys(range(0, 101), "")
-
-# files = glob.glob(os.path.join(datasetFolder, "*.csv"))
-# files.sort()
-
-# for csv in files:
-#     hash = hashlib.sha256(open(csv, "rb").read()).hexdigest()
-#     if hash not in hashes.values():
-#         hashes[i % 101] = hash
-#         cleanCSV(os.path.basename(csv))
-#         i += 1
 
 spark.stop()
