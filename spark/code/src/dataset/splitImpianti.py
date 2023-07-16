aa3023fb5039796746d6445fe2000ca302049b9c
SO4REN <ale.catinello@gmail.com>
2023-07-16 18:12:36 +0200

    chore: change dataset folder and splitting

--- spark/code/src/dataset/splitImpianti.py
+++ spark/code/src/dataset/splitImpianti.py
@@ -1,5 +1,6 @@
 import glob
 import pandas as pd
+import os
 
 
 
@@ -7,7 +8,7 @@ def cleanPrezzi(df, anagrafica):
     df = df[df.isSelf == 1]
     df = df[df.descCarburante.isin(["Benzina", "Gasolio"])]
     df = df.merge(anagrafica, on="idImpianto", how="inner")
-    df.carburante = df.carburante.map({"Benzina": 0, "Gasolio": 1})
+    df.descCarburante = df.descCarburante.map({"Benzina": 0, "Gasolio": 1})
     df = df.drop(["isSelf", "Provincia", "dtComu", "Tipo Impianto"], axis=1)
     return df
 
@@ -15,19 +16,22 @@ def removeFirstLine(files):
     for file in files:
         with open(file, "r") as f:
             data = f.readlines()
-            data = data[2:]
             
+            if not data[0].startswith("Estrazione"):
+                return
+            
+            data = data[2:]
             with open(file, "w") as f:
                 f.writelines(data)
 
 
-def main():
+def toSingleFile():
     anagrafica = pd.read_parquet("anagrafica_impianti_CT.parquet")
     impianti = anagrafica.idImpianto.unique()
 
     df = pd.DataFrame(columns=["idImpianto", "carburante", "X_prezzo", "Y_prezzo"])
 
-    files = glob.glob("Prezzi/*.csv")
+    files = glob.glob("../../PrezziRaw/*.csv")
     files.sort()
 
     removeFirstLine(files)
@@ -46,16 +50,63 @@ def main():
             if prezzoX.empty or prezzoY.empty:
                     continue
             else:
-                for carb in ("Benzina", "Gasolio"):
+                for carb in (0, 1):
+                    xTarget = prezzoX[prezzoX.descCarburante == carb].prezzo.values[0] if not prezzoX[prezzoX.descCarburante == carb].empty else -1
+                    yTarget = prezzoY[prezzoY.descCarburante == carb].prezzo.values[0] if not prezzoY[prezzoY.descCarburante == carb].empty else -1
+                    
                     newRow = pd.DataFrame({
                                         "idImpianto": impianto,
                                         "carburante": carb,
-                                        "X_prezzo": prezzoX[prezzoX.descCarburante == carb].prezzo.values[0],
-                                        "Y_prezzo": prezzoY[prezzoY.descCarburante == carb].prezzo.values[0]},
+                                        "X_prezzo": xTarget,
+                                        "Y_prezzo": yTarget},
                                         columns=["idImpianto", "carburante", "X_prezzo", "Y_prezzo"], index=[0])
                     df = pd.concat([df, newRow], ignore_index=True)
     df.to_parquet("Prezzi.parquet")
+    
+def toMultipleFiles():
+    anagrafica = pd.read_parquet("anagrafica_impianti_CT.parquet")
+    impianti = anagrafica.idImpianto.unique()
+
+    # df = pd.DataFrame(columns=["idImpianto", "carburante", "X_prezzo", "Y_prezzo"])
+
+    files = glob.glob("../../PrezziRaw/*.csv")
+    files.sort()
+
+    removeFirstLine(files)
+
+    for X, Y in zip(files, files[1:]):
+        dfX = pd.read_csv(X, sep=";", header=0, names=["idImpianto", "descCarburante", "prezzo", "isSelf", "dtComu"], on_bad_lines="skip")
+        dfY = pd.read_csv(Y, sep=";", header=0, names=["idImpianto", "descCarburante", "prezzo", "isSelf", "dtComu"], on_bad_lines="skip")
+
+        dfX = cleanPrezzi(dfX, anagrafica)
+        dfY = cleanPrezzi(dfY, anagrafica)
+
+        for impianto in impianti:
+            df = None
+            if os.path.exists("Prezzi/{id}.parquet".format(id=impianto)):
+                df = pd.read_parquet("Prezzi/{id}.parquet".format(id=impianto))
+            else:
+                df = pd.DataFrame(columns=["carburante", "X_prezzo", "Y_prezzo"])
+            
+            prezzoX = dfX[dfX.idImpianto == impianto][["descCarburante", "prezzo", "idImpianto"]]
+            prezzoY = dfY[dfY.idImpianto == impianto][["descCarburante", "prezzo", "idImpianto"]]
+
+            if prezzoX.empty or prezzoY.empty:
+                    continue
+            else:
+                for carb in (0, 1):
+                    xTarget = prezzoX[prezzoX.descCarburante == carb].prezzo.values[0] if not prezzoX[prezzoX.descCarburante == carb].empty else -1
+                    yTarget = prezzoY[prezzoY.descCarburante == carb].prezzo.values[0] if not prezzoY[prezzoY.descCarburante == carb].empty else -1
+                    
+                    newRow = pd.DataFrame({
+                                        "carburante": carb,
+                                        "X_prezzo": xTarget,
+                                        "Y_prezzo": yTarget},
+                                        columns=["carburante", "X_prezzo", "Y_prezzo"], index=[0])
+                    df = pd.concat([df, newRow], ignore_index=True)
+            df.to_parquet("Prezzi/{id}.parquet".format(id=impianto))
 
 
 if __name__ == "__main__":
-    main()
+    # toSingleFile()
+    toMultipleFiles()
