f35ae7a4e8b7a10186b78881998ac55fe96a7e60
SO4REN <ale.catinello@gmail.com>
2023-07-16 22:43:50 +0200

    feat: use two regressors, one for each fuel

--- spark/code/src/predict.py
+++ spark/code/src/predict.py
@@ -5,11 +5,11 @@ from pyspark.ml.feature import VectorAssembler, OneHotEncoder
 
 
 def train(trainDF):
-    encoder = OneHotEncoder(inputCols=["idImpianto", "carburante"], outputCols=["X_idImpianto", "X_carburante"])
+    encoder = OneHotEncoder(inputCols=["idImpianto"], outputCols=["X_idImpianto"])
     model = encoder.fit(trainDF)
     df = model.transform(trainDF)
     
-    assembler = VectorAssembler(inputCols=["X_prezzo", "X_idImpianto", "X_carburante"], outputCol="features")
+    assembler = VectorAssembler(inputCols=["X_prezzo", "X_idImpianto"], outputCol="features")
     df = assembler.transform(df)
 
     lr = DecisionTreeRegressor(featuresCol="features", labelCol="Y_prezzo", predictionCol="prediction")
@@ -17,18 +17,27 @@ def train(trainDF):
     return regressor
 
 
-def predictStreamingDF(df, trainDF):
-    regressor = train(trainDF)
-
-    encoder = OneHotEncoder(inputCols=["idImpianto", "carburante"], outputCols=["X_idImpianto", "X_carburante"])
-    model = encoder.fit(trainDF)
-    df = model.transform(df)
-
-    assembler = VectorAssembler(inputCols=["prezzo", "X_idImpianto", "X_carburante"], outputCol="features")
-    df = assembler.transform(df)
-    df = regressor.transform(df)
+def predictStreamingDF(df, trainDF):    
+    toRet = None
+    assembler = VectorAssembler(inputCols=["prezzo", "X_idImpianto"], outputCol="features")
     
-    df = df.drop("X_idImpianto", "features", "X_carburante")
-    df = df.withColumn("@timestamp", fun.date_format(fun.current_timestamp(), "yyyy-MM-dd HH:mm:ss"))
-    df = df.withColumn("carburante", fun.when(df.carburante == 0, "Benzina").otherwise("Gasolio"))
-    return df
+    for carb in (0, 1):
+        tempTrainDF = trainDF.filter(trainDF.carburante == carb)
+        regressor = train(tempTrainDF)
+
+        tempDF = df.filter(df.carburante == carb)
+
+        encoder = OneHotEncoder(inputCols=["idImpianto"], outputCols=["X_idImpianto"])
+        model = encoder.fit(tempTrainDF)
+        tempDF = model.transform(tempDF)
+
+        tempDF = assembler.transform(tempDF)
+        tempDF = regressor.transform(tempDF)
+
+        tempDF = tempDF.drop("X_idImpianto", "features")
+
+        toRet = tempDF if toRet is None else toRet.union(tempDF)
+
+    toRet = toRet.withColumn("@timestamp", fun.date_format(fun.current_timestamp(), "yyyy-MM-dd HH:mm:ss"))
+    toRet = toRet.withColumn("carburante", fun.when(toRet.carburante == 0, "Benzina").otherwise("Gasolio"))
+    return toRet
